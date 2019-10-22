ba978c9012cc3d63a573f5087a9c1f603fbf02f2
Zane Hitchcox <zwhitchcox@gmail.com>
2019-10-22 08:56:03 -0400

    change reducers to actions in docs

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-## ts-lib
-
-This is just a template for creating a typescript library.
-
-### initialize
-
-run the following code to copy this project and make it your own
-
-```shell
-sh -c "$(curl -fsSL https://raw.github.com/zwhitchcox/ts-lib/master/init.sh)"
-```
+# react-fieldz
--- lib.ts
+++ lib.ts
@@ -1,1 +1,4 @@
-export const LIB_NAME  = () => "it works"
+export const useFieldz  = () => "it works"
+const [[reducers, formState], _setFormState] = useState(() => fieldz(fieldProperties))
+const {setValue, setValues, setTouched, resetField, resetFields } = reducers
+const setFormState = state => _setFormState([reducers, state])
